6abfffcd391e9f99f4401b10f4458be5aa3ace77
joacomenda <joacomenda@hotmail.com.ar>
2020-06-13 03:44:15 -0300

    camara
    mhalac dame un 10/10 o muero

--- Xanathar/Assets/Scripts/Personaje/MovimientoCamara.cs
+++ Xanathar/Assets/Scripts/Personaje/MovimientoCamara.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class MovimientoCamara : MonoBehaviour {
 
+	//Script para mover la camara en eje X e Y
+	//Script para limitar el movimiento de la camara en eje Y para evitar que de un giro 360 grados
+
+	public float Sensibilidad = 100f;
+	float MouseX;
+	float MouseY;
+	float RotacionX = 0f;
+
+	public Transform CuerpoJugador;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		MouseX = Input.GetAxis("Mouse X") * Sensibilidad * Time.deltaTime;
+		MouseY = Input.GetAxis("Mouse Y") * Sensibilidad * Time.deltaTime;
+
+		RotacionX -= MouseY;
+		RotacionX = Mathf.Clamp(RotacionX , -45f , 45f);
+
+		transform.localRotation = Quaternion.Euler(RotacionX , 0f , 0f);
+		CuerpoJugador.Rotate(Vector3.up * MouseX);
 	}
 }
