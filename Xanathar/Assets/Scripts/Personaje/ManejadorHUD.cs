0f5a028bf4002e510a07fc07ba4bfa045a28ef0c
joacomenda <joacomenda@hotmail.com.ar>
2020-06-16 03:03:22 -0300

    Personaje con HUD
    No terminado ni de cerca, pero ahora si apretas la i, te muestra las pasivas y estadisticas y si la apretas devuelta, se ocultan

--- Xanathar/Assets/Scripts/Personaje/ManejadorHUD.cs
+++ Xanathar/Assets/Scripts/Personaje/ManejadorHUD.cs
@@ -5,13 +5,28 @@ using UnityEngine.UI;
 
 public class ManejadorHUD : MonoBehaviour {
 
-	void Start () {
+	public GameObject Pasivas;
+	public GameObject Estadisticas;
+	int Contador = 0;
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-
+		if(Input.GetKeyDown(KeyCode.I) && Contador == 0)
+		{
+			Pasivas.SetActive(false);
+			Estadisticas.SetActive(false);
+			Contador +=1;
+		}
+		else if(Input.GetKeyDown(KeyCode.I) && Contador == 1)
+		{
+			Pasivas.SetActive(true);
+			Estadisticas.SetActive(true);
+			Contador = 0;
+		}
 	}
 }
