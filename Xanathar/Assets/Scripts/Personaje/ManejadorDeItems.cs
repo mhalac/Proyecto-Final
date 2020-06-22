186dab53ae460b6c047210f0a69efb9f5b8df950
joacomenda <joacomenda@hotmail.com.ar>
2020-06-22 01:14:37 -0300

    HUD 1/3 completado
    Todavia falta que se actualizen las estadisticas y pasivas, ahora hay un cartel que te muestra el nombre, la descripcion y la categoria del Item.
    Falta que te muestre la vida en pantalla

--- Xanathar/Assets/Scripts/Personaje/ManejadorDeItems.cs
+++ Xanathar/Assets/Scripts/Personaje/ManejadorDeItems.cs
@@ -12,25 +12,16 @@ public class ManejadorDeItems : MonoBehaviour {
 	int layerMask2 = 1 << 15;
 	int layerMask3 = 1 << 16;
 	int layerMask4 = 1 << 17;
-	int layermasFinal;
+	int layermaskFinal;
 	//Rango Del raycast
 	float Rango = 5;
 
 	//Variables para ocultar y mostrar el HUD
 	static bool OcultadorDeHud = false;
-	public GameObject HUDPasivas;
-	public GameObject HUDEstadisticas;
-
-
-	//Referencias a las Activas del juego
-	private GameObject ActivaDeFuego1;
-	private GameObject ActivaDeFuego2;
-	private GameObject ActivaDeViento1;
-	private GameObject ActivaDeViento2;
-	private GameObject ActivaDeTierra1;
-	private GameObject ActivaDeTierra2;
-	private GameObject ActivaDeAgua1;
-	private GameObject ActivaDeAgua2;
+	private bool OcultadorDeMensaje = false;
+	public CanvasGroup HUDPasivas;
+	public CanvasGroup HUDEstadisticas;
+	public CanvasGroup MensajeNotificador;
 
 	//Referencias a los slots de Activas del HUD
 	private GameObject SlotActivaDeFuego;
@@ -38,31 +29,45 @@ public class ManejadorDeItems : MonoBehaviour {
 	private GameObject SlotActivaDeTierra;
 	private GameObject SlotActivaDeAgua;
 
+	//Notifico las variables para reemplazar su texto mas adelante
+	private GameObject NombreNotificador;
+	private GameObject CategoriaNotificador;
+	private GameObject DescripcionNotificador;
+
+
+	//Instanciador de donde nuestros objetos apareceran
+	private GameObject Instanciador;
+
 	//Lista De Activas que nos permite chequear si los slots estan llenos
+	//Fuego , Viento , Tierra, Agua
 	static bool[] ArrayDeActivas = new bool[]{ false, false, false, false };
+
+	int OrdenDeLayers = 0;
+
+	GameObject[] Activas;
 
 	// Use this for initialization
 	void Start ()
 	{
-		layermasFinal = layerMask1 | layerMask2 | layerMask3 | layerMask4;
-		//Encuentro los prefabs de las activas y accedo a sus variables mas adelante
-		ActivaDeFuego1 = Resources.Load<GameObject>("Activas/ActivaDeFuego1");
-		ActivaDeFuego2 = Resources.Load<GameObject>("Activas/ActivaDeFuego2");
+		MensajeNotificador.alpha = 0f;
+		//Fusiono las layermasks en una sola
+		layermaskFinal = layerMask1 | layerMask2 | layerMask3 | layerMask4;
 
-		ActivaDeViento1 = Resources.Load<GameObject>("Activas/ActivaDeViento1");
-		ActivaDeViento2 = Resources.Load<GameObject>("Activas/ActivaDeViento2");
-
-		ActivaDeTierra1 = Resources.Load<GameObject>("Activas/ActivaDeTierra1");
-		ActivaDeTierra2 = Resources.Load<GameObject>("Activas/ActivaDeTierra2");
-
-		ActivaDeAgua1 = Resources.Load<GameObject>("Activas/ActivaDeAgua1");
-		ActivaDeAgua2 = Resources.Load<GameObject>("Activas/ActivaDeAgua2");
+		//Lleno mi Array de los items activos
+		Activas = Resources.LoadAll<GameObject>("Activas");
 
 		//Encuentro el Slot para referenciarlo y acceder a sus variables mas adelante
 		SlotActivaDeFuego = GameObject.Find("ActivaFuego");
 		SlotActivaDeViento = GameObject.Find("ActivaViento");
 		SlotActivaDeTierra = GameObject.Find("ActivaTierra");
 		SlotActivaDeAgua = GameObject.Find("ActivaAgua");
+
+		//Encuentro los campos notifcadores para acceder a sus variables mas adelante
+		NombreNotificador = GameObject.Find("NombreDelItem");
+		CategoriaNotificador = GameObject.Find("CategoriaDelItem");
+		DescripcionNotificador = GameObject.Find("DescripcionDelItem");
+
+		Instanciador = GameObject.Find("Instanciador");
 	}
 	
 	// Update is called once per frame
@@ -80,6 +85,12 @@ public class ManejadorDeItems : MonoBehaviour {
 
 	private void ActivadorDeHUD()
 	{
+		if(Input.GetKeyDown(KeyCode.P) && OcultadorDeMensaje == true)
+		{
+			OcultadorDeMensaje = false;
+			MensajeNotificador.alpha = 0f;
+		}
+		
 		if(Input.GetKeyDown(KeyCode.I))
 		{
 			OcultadorDeHud = !OcultadorDeHud;
@@ -87,156 +98,221 @@ public class ManejadorDeItems : MonoBehaviour {
 
 		if(OcultadorDeHud == true)
 		{
-			HUDPasivas.SetActive(true);
-			HUDEstadisticas.SetActive(true);
+			HUDPasivas.alpha = 0f;
+			HUDEstadisticas.alpha = 0f;
 		}
 		else
 		{
-			HUDPasivas.SetActive(false);
-			HUDEstadisticas.SetActive(false);
+			HUDPasivas.alpha = 1f;
+			HUDEstadisticas.alpha = 1f;
 		}
 		
 	}
 
 	private void RaycastItems()
 	{
-		if(Input.GetKeyDown(KeyCode.E) && Physics.Raycast(Camera.main.transform.position , Camera.main.transform.forward , out DondeToco , Rango , layermasFinal))
+		if(Input.GetKeyDown(KeyCode.E) && Physics.Raycast(Camera.main.transform.position , Camera.main.transform.forward , out DondeToco , Rango , layermaskFinal))
 		{
 			EquipadorActivas();
 		}
 	}
 
+	private void BuscadorDeLayer()
+	{
+		int BuscadorDeLayer = DondeToco.collider.gameObject.layer;
+
+		switch (BuscadorDeLayer)
+		{
+			case 14:
+			OrdenDeLayers = 3;
+			break;
+
+			case 15:
+			OrdenDeLayers = 0;
+			break;
+
+			case 16:
+			OrdenDeLayers = 2;
+			break;
+
+			case 17:
+			OrdenDeLayers = 1;
+			break;
+
+			default:
+			print("ERROR LAYER NO ENCONTRADA");
+			break;
+		}
+	}
+
 	private void EquipadorActivas()
 	{
-		string Comparador = DondeToco.collider.tag;
-		switch(Comparador)
+		GameObject[] GuardadorDeItems = new GameObject[2];
+		BuscadorDeLayer();
+
+		string Buscador = DondeToco.collider.tag;
+		GameObject ContenedorDeGameObjects = null;
+
+		for(int i = 0; i < Activas.Length; i++)
+		{
+			if(Activas[i].tag != DondeToco.collider.gameObject.tag && Activas[i].layer == DondeToco.collider.gameObject.layer)
+			{
+				GuardadorDeItems[1] = Activas[i];
+			}
+		}
+
+		for(int i = 0; i < Activas.Length; i++)
+		{
+			if(Activas[i].tag == DondeToco.collider.tag)
+			{
+				ContenedorDeGameObjects = Activas[i];
+			}
+		}
+
+		switch (Buscador)
 		{
 			case "ActivaDeFuego1":
 
-			if(ArrayDeActivas[0] == false)
+			if(ArrayDeActivas[OrdenDeLayers] == false)
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeFuego.GetComponent<Image>().sprite = ActivaDeFuego1.GetComponent<InformacionDeItems>().Icono;
-				ArrayDeActivas[0] = true;
+				SlotActivaDeFuego.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				ArrayDeActivas[OrdenDeLayers] = true;
 			}
 			else
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeFuego.GetComponent<Image>().sprite = ActivaDeFuego1.GetComponent<InformacionDeItems>().Icono;
-				Instantiate(ActivaDeFuego2 , transform.position , Quaternion.identity);
+				SlotActivaDeFuego.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				Instantiate(GuardadorDeItems[1] , Instanciador.transform.position , Quaternion.identity);
 			}
 			break;
 
 			case "ActivaDeFuego2":
 
-			if(ArrayDeActivas[0] == false)
+			if(ArrayDeActivas[OrdenDeLayers] == false)
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeFuego.GetComponent<Image>().sprite = ActivaDeFuego2.GetComponent<InformacionDeItems>().Icono;
-				ArrayDeActivas[0] = true;
+				SlotActivaDeFuego.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				ArrayDeActivas[OrdenDeLayers] = true;
 			}
 			else
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeFuego.GetComponent<Image>().sprite = ActivaDeFuego2.GetComponent<InformacionDeItems>().Icono;
-				Instantiate(ActivaDeFuego1 , transform.position , Quaternion.identity);
+				SlotActivaDeFuego.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				Instantiate(GuardadorDeItems[1] , Instanciador.transform.position , Quaternion.identity);
 			}
 			break;
 
 			case "ActivaDeViento1":
 
-			if(ArrayDeActivas[1] == false)
+			if(ArrayDeActivas[OrdenDeLayers] == false)
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeViento.GetComponent<Image>().sprite = ActivaDeViento1.GetComponent<InformacionDeItems>().Icono;
-				ArrayDeActivas[1] = true;
+				SlotActivaDeViento.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				ArrayDeActivas[OrdenDeLayers] = true;
 			}
 			else
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeViento.GetComponent<Image>().sprite = ActivaDeViento1.GetComponent<InformacionDeItems>().Icono;
-				Instantiate(ActivaDeViento2 , transform.position , Quaternion.identity);
+				SlotActivaDeViento.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				Instantiate(GuardadorDeItems[1] , Instanciador.transform.position , Quaternion.identity);
 			}
 			break;
 
 			case "ActivaDeViento2":
 
-			if(ArrayDeActivas[1] == false)
+			if(ArrayDeActivas[OrdenDeLayers] == false)
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeViento.GetComponent<Image>().sprite = ActivaDeViento2.GetComponent<InformacionDeItems>().Icono;
-				ArrayDeActivas[1] = true;
+				SlotActivaDeViento.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				ArrayDeActivas[OrdenDeLayers] = true;
 			}
 			else
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeViento.GetComponent<Image>().sprite = ActivaDeViento2.GetComponent<InformacionDeItems>().Icono;
-				Instantiate(ActivaDeViento1 , transform.position , Quaternion.identity);
+				SlotActivaDeViento.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				Instantiate(GuardadorDeItems[1] , Instanciador.transform.position , Quaternion.identity);
 			}
 			break;
 
 			case "ActivaDeTierra1":
-			if(ArrayDeActivas[2] == false)
+
+			if(ArrayDeActivas[OrdenDeLayers] == false)
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeTierra.GetComponent<Image>().sprite = ActivaDeTierra1.GetComponent<InformacionDeItems>().Icono;
-				ArrayDeActivas[2] = true;
+				SlotActivaDeTierra.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				ArrayDeActivas[OrdenDeLayers] = true;
 			}
 			else
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeTierra.GetComponent<Image>().sprite = ActivaDeTierra1.GetComponent<InformacionDeItems>().Icono;
-				Instantiate(ActivaDeTierra2 , transform.position , Quaternion.identity);
+				SlotActivaDeTierra.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				Instantiate(GuardadorDeItems[1] , Instanciador.transform.position , Quaternion.identity);
 			}
 			break;
 
 			case "ActivaDeTierra2":
-			if(ArrayDeActivas[2] == false)
+
+			if(ArrayDeActivas[OrdenDeLayers] == false)
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeTierra.GetComponent<Image>().sprite = ActivaDeTierra2.GetComponent<InformacionDeItems>().Icono;
-				ArrayDeActivas[2] = true;
+				SlotActivaDeTierra.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				ArrayDeActivas[OrdenDeLayers] = true;
 			}
 			else
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeTierra.GetComponent<Image>().sprite = ActivaDeTierra2.GetComponent<InformacionDeItems>().Icono;
-				Instantiate(ActivaDeTierra1 , transform.position , Quaternion.identity);
+				SlotActivaDeTierra.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				Instantiate(GuardadorDeItems[1] , Instanciador.transform.position , Quaternion.identity);
 			}
 			break;
 
-
 			case "ActivaDeAgua1":
 
-			if(ArrayDeActivas[3] == false)
+			if(ArrayDeActivas[OrdenDeLayers] == false)
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeAgua.GetComponent<Image>().sprite = ActivaDeAgua1.GetComponent<InformacionDeItems>().Icono;
-				ArrayDeActivas[3] = true;
+				SlotActivaDeAgua.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				ArrayDeActivas[OrdenDeLayers] = true;
 			}
 			else
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeAgua.GetComponent<Image>().sprite = ActivaDeAgua1.GetComponent<InformacionDeItems>().Icono;
-				Instantiate(ActivaDeAgua2 , transform.position , Quaternion.identity);
+				SlotActivaDeAgua.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				Instantiate(GuardadorDeItems[1] , Instanciador.transform.position , Quaternion.identity);
 			}
 			break;
 
 			case "ActivaDeAgua2":
 
-			if(ArrayDeActivas[3] == false)
+			if(ArrayDeActivas[OrdenDeLayers] == false)
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeAgua.GetComponent<Image>().sprite = ActivaDeAgua2.GetComponent<InformacionDeItems>().Icono;
-				ArrayDeActivas[3] = true;
+				SlotActivaDeAgua.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				ArrayDeActivas[OrdenDeLayers] = true;
 			}
 			else
 			{
 				Destroy(DondeToco.collider.gameObject);
-				SlotActivaDeAgua.GetComponent<Image>().sprite = ActivaDeAgua2.GetComponent<InformacionDeItems>().Icono;
-				Instantiate(ActivaDeAgua1 , transform.position , Quaternion.identity);
+				SlotActivaDeAgua.GetComponent<Image>().sprite = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Icono;
+				Instantiate(GuardadorDeItems[1] , Instanciador.transform.position , Quaternion.identity);
 			}
 			break;
+
 		}
+
+		Text TextoNombre = NombreNotificador.GetComponent<Text>();
+		TextoNombre.text = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Nombre;
+
+		Text TextoCategoria = CategoriaNotificador.GetComponent<Text>();
+		TextoCategoria.text = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Categoria;
+
+		Text TextoDescripcion = DescripcionNotificador.GetComponent<Text>();
+		TextoDescripcion.text = ContenedorDeGameObjects.GetComponent<InformacionDeItems>().Descripcion;
+
+		OcultadorDeMensaje = true;
+
+		MensajeNotificador.alpha = 1f;
+
 	}
 }
