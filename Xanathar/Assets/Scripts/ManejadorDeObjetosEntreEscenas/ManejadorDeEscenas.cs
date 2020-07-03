3e3bccd0d49b7b1a9c75df7687164e3f782d3424
joacomenda <joacomenda@hotmail.com.ar>
2020-07-03 19:03:41 -0300

    Manejo de Niveles en Progreso

--- Xanathar/Assets/Scripts/ManejadorDeObjetosEntreEscenas/ManejadorDeEscenas.cs
+++ Xanathar/Assets/Scripts/ManejadorDeObjetosEntreEscenas/ManejadorDeEscenas.cs
@@ -5,22 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class ManejadorDeEscenas : MonoBehaviour {
 
+	[Header("Escenas Nivel De Fuego")]
+	public Object[] NivelDeFuego = new Object[4];
+	int ReferenciadorDeEscenas = 0;
+	string NombreDeEscenas;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 
 	void OnCollisionEnter(Collision col)
 	{
-		if(col.gameObject.name == "EntradaNivelFuego")
+		if(col.gameObject.name == "EntradaNivelFuego" && ReferenciadorDeEscenas == 0)
 		{
-			print("hola");
-			SceneManager.LoadScene("SubNivel1");
+			NombreDeEscenas = NivelDeFuego[0].name;
+			SceneManager.LoadScene(NombreDeEscenas);
+			ReferenciadorDeEscenas += 1;
+			print(ReferenciadorDeEscenas);
 		}
+		/*
+		BuscadorDeEscenas = 0;
+		string bru = NivelDeFuego[0].name.ToString();
+		print(bru);
+		SceneManager.LoadScene(bru);
+		*/
 	}
 }
