c8637d5fb0fb119f28dae4d4e22864e11b3ca269
joacomenda <joacomenda@hotmail.com.ar>
2020-07-31 17:29:30 -0300

    ._.XD

--- Xanathar/Assets/Scripts/Jefes/BarraDeVidaJefe.cs
+++ Xanathar/Assets/Scripts/Jefes/BarraDeVidaJefe.cs
@@ -1,35 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class BarraDeVidaJefe : MonoBehaviour {
 
 	//Cuando se inicie el script el valor actual de vida debe ser igual al valor maximo de vida
 	public float ValorDeVidaMaxima;
+	//a ValorDeVidaActual metele la vida del jefe
 	public float ValorDeVidaActual;
 	private float RellenoDeVida;
-
 	public Image ContenidoBarraDeVida;
 
 	// Use this for initialization
 	void Start ()
 	{
-		ValorDeVidaActual = ValorDeVidaMaxima;
+		//Igualas la vida maxzima a la vida actual
+		ValorDeVidaMaxima = ValorDeVidaActual;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		ManejadorDeVidaJefe(ValorDeVidaMaxima , ValorDeVidaActual);
+		ManejadorDeVidaJefe(ValorDeVidaMaxima);
 	}
 
-	public void ManejadorDeVidaJefe(float VidaMaximaDelJefe , float VidaActualDelJefe)
+	public void ManejadorDeVidaJefe(float VidaMaximaDelJefe)
 	{
-		RellenoDeVida = VidaActualDelJefe / VidaMaximaDelJefe;
+		RellenoDeVida = ValorDeVidaActual / VidaMaximaDelJefe;
 		ContenidoBarraDeVida.fillAmount = RellenoDeVida;
 
-		if(VidaActualDelJefe <= 0)
+		if(ValorDeVidaActual <= 0)
 		{
 			Debug.Log("El jefe se murio");
 		}
