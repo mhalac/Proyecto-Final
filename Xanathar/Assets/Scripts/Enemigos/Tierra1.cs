7e642a524c6a1db755176d2b18bc231b4230098f
joacomenda <joacomenda@hotmail.com.ar>
2020-08-19 01:05:30 -0300

    o

--- Xanathar/Assets/Scripts/Enemigos/Tierra1.cs
+++ Xanathar/Assets/Scripts/Enemigos/Tierra1.cs
@@ -9,12 +9,11 @@ public class Tierra1 : MonoBehaviour {
 	private Transform UltimaPosicion;
 
 	[Header("Transforms Seleccionables")]
-	public Transform VisionDeDisparo;
+
 	public Transform RangoMinimo;
 
 	[Header("Parametros")]
 	public float AreaIdle;
-	public string NombreHijo;
 	public int AlcanceMaximo;
 	public string EstadoActual;
 	private NavMeshAgent Agente;
@@ -26,7 +25,7 @@ public class Tierra1 : MonoBehaviour {
 	private Vector3 PosicionEnSpawn;
 	private float DelayInicial;
 	public float Damage;
-	private Transform Heredar;
+	public Transform Heredar;
 	private Vector3 Destino;
 	public float Delay;
 	private int EMask;
@@ -37,8 +36,14 @@ public class Tierra1 : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-		Heredar = transform.Find(NombreHijo).GetComponent<Transform>();
-		Debug.Log(Heredar.name);
+		PosicionEnSpawn = Heredar.position;
+		Agente = GetComponent<NavMeshAgent>();
+		RangoMinimo.position = new Vector3(Heredar.position.x , Heredar.position.y , + AlcanceMaximo);
+		EstadoActual = Estados[0];
+		DelayInicial = Delay;
+
+		PMask = LayerMask.NameToLayer("Personaje");
+		EMask = LayerMask.NameToLayer("Enemigo");
 	}
 	
 	// Update is called once per frame
