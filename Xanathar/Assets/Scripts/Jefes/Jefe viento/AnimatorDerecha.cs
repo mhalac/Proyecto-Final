bb133661477da1a5e8cf8fb60e6262e65afd31e3
mhalac <matiasmatute100@gmail.com>
2020-08-21 02:17:24 -0300

    Progresos con el jefe de viento y sus fases y animaciones xd

---
+++ Xanathar/Assets/Scripts/Jefes/Jefe viento/AnimatorDerecha.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorDerecha : MonoBehaviour
+{
+    private JefeViento Jefe;
+
+    // Use this for initialization
+    void Start()
+    {
+        Jefe = FindObjectOfType<JefeViento>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void EmpezoAAplastar()
+    {
+        Jefe.EstaAplastando = true;
+    }
+    public void TerminoDeAplastar()
+    {
+        Jefe.EstaAplastando = false;
+
+    }
+	
+  
+}
