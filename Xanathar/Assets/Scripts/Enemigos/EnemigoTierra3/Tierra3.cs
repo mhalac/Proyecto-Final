809eef155f1a9fdc4690907b06865793f3a5f836
joacomenda <joacomenda@hotmail.com.ar>
2020-08-23 17:30:57 -0300

    Cambios en el enemigo de tierra lvl3

---
+++ Xanathar/Assets/Scripts/Enemigos/EnemigoTierra3/Tierra3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tierra3 : MonoBehaviour {
+
+	private RaycastHit Hit;
+
+	[Header("Transforms Seleccionables")]
+	public Transform Heredar;
+	public Transform PuntoDeAtaque;
+	public Transform AreaDeVision;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
