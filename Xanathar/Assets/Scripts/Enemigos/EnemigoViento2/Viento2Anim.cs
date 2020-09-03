ed332f5991c6a436c3e29da257fb57af37e9c834
joacomenda <joacomenda@hotmail.com.ar>
2020-09-03 01:36:22 -0300

    Enemigo de tierra 2 empezado

---
+++ Xanathar/Assets/Scripts/Enemigos/EnemigoViento2/Viento2Anim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viento2Anim : MonoBehaviour
+{
+    public Viento2 Padre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void IrALugarRandom()
+    {
+        Padre.IrAPosRandom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
