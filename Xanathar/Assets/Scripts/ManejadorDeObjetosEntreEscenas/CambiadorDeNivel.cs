1197fd841a6f0bf49ded269dc8398f860171ef62
mhalac <matiasmatute100@gmail.com>
2020-10-25 14:26:48 -0300

    xdxd

--- Xanathar/Assets/Scripts/ManejadorDeObjetosEntreEscenas/CambiadorDeNivel.cs
+++ Xanathar/Assets/Scripts/ManejadorDeObjetosEntreEscenas/CambiadorDeNivel.cs
@@ -2,108 +2,120 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class CambiadorDeNivel : MonoBehaviour {
+public class CambiadorDeNivel : MonoBehaviour
+{
 
-	public bool PermitirCambios = false;
-	public Animator AnimacionDeCambioDeNivel;
-	public BuscadorDePos BuscadorDePos;
-	AsyncOperation asyncOperation;
-	public void IniciadorDeCambio()
-	{
-		if(PermitirCambios == false)
-		{
-			PermitirCambios = true;
-			AnimacionDeCambioDeNivel.SetTrigger("Desaparecer");
-			ManejadorDeEscenas.ActivadorDeCambio = true;
-		}
-	}
+    public bool PermitirCambios = false;
+    public Animator AnimacionDeCambioDeNivel;
+    public BuscadorDePos BuscadorDePos;
+    AsyncOperation asyncOperation;
+    public void IniciadorDeCambio()
+    {
+        if (PermitirCambios == false)
+        {
+            PermitirCambios = true;
+            AnimacionDeCambioDeNivel.SetTrigger("Desaparecer");
+            ManejadorDeEscenas.ActivadorDeCambio = true;
+        }
+    }
 
-	IEnumerator CargarEscena()
-	{
-		asyncOperation = SceneManager.LoadSceneAsync (ManejadorDeEscenas.NombreDeEscena , LoadSceneMode.Additive);
+    IEnumerator CargarEscena()
+    {
+        asyncOperation = SceneManager.LoadSceneAsync(ManejadorDeEscenas.NombreDeEscena, LoadSceneMode.Additive);
 
-		asyncOperation.allowSceneActivation = true;
-		yield return asyncOperation;
-		StartCoroutine(BorrarEscena());
-	}
+        asyncOperation.allowSceneActivation = true;
+        yield return asyncOperation;
+        StartCoroutine(BorrarEscena());
+    }
 
-	IEnumerator BorrarEscena()
-	{
-		string EscenaActual = SceneManager.GetActiveScene().name;
-		asyncOperation = SceneManager.UnloadSceneAsync(EscenaActual);
+    IEnumerator BorrarEscena()
+    {
+        string EscenaActual = SceneManager.GetActiveScene().name;
+        asyncOperation = SceneManager.UnloadSceneAsync(EscenaActual);
 
-		asyncOperation.allowSceneActivation = true;
-		while(!asyncOperation.isDone)
-		{
-			AnimacionDeCambioDeNivel.SetTrigger("Aparecer");
-			ManejadorDeEscenas.ActivadorDeCambio = false;
-			yield return asyncOperation;
+        asyncOperation.allowSceneActivation = true;
+        while (!asyncOperation.isDone)
+        {
+            AnimacionDeCambioDeNivel.SetTrigger("Aparecer");
+            ManejadorDeEscenas.ActivadorDeCambio = false;
+            yield return asyncOperation;
 
 
-			BuscadorDePos.ManejadorDePos();
-			PermitirCambios = false;
-			MuerteEnVacios.Caiste = false;
+            BuscadorDePos.ManejadorDePos();
+            PermitirCambios = false;
+            MuerteEnVacios.Caiste = false;
 
-			PostProcessManager CambiarPostPros = FindObjectOfType<PostProcessManager>();
-			CambiarPostPros.CambiarElPPL(SceneManager.GetActiveScene().name);
+            PostProcessManager CambiarPostPros = FindObjectOfType<PostProcessManager>();
+            CambiarPostPros.CambiarElPPL(SceneManager.GetActiveScene().name);
 
-			if(SceneManager.GetActiveScene().name == "Lobby")
-			{
-				if(ControlarPuertasYJefes.JefeEliminado == true)
-				{
-					EstadisticasDePersonaje estadisticasDePersonaje = FindObjectOfType<EstadisticasDePersonaje>();
+            if (SceneManager.GetActiveScene().name == "Lobby")
+            {
+                if (ControlarPuertasYJefes.JefeEliminado == true)
+                {
+                    EstadisticasDePersonaje estadisticasDePersonaje = FindObjectOfType<EstadisticasDePersonaje>();
 
-					if(estadisticasDePersonaje.MurioDespuesDeMatarJefe == true)
-					{
-						AgarradorDeItems agarradorDeItems = FindObjectOfType<AgarradorDeItems>();
-						DropearItemsEnSpawn.TirarItemsEnSpwn(agarradorDeItems.CopiaDeObjetos);
-						ControlarPuertasYJefes.JefeEliminado = false;
-						estadisticasDePersonaje.MurioDespuesDeMatarJefe = false;
-						
-					}
-					else
-					{
-						ControlarPuertasYJefes.JefeEliminado = false;
-					}
-				}
-				
+                    if (estadisticasDePersonaje.MurioDespuesDeMatarJefe == true)
+                    {
+                        AgarradorDeItems agarradorDeItems = FindObjectOfType<AgarradorDeItems>();
+                        DropearItemsEnSpawn.TirarItemsEnSpwn(agarradorDeItems.CopiaDeObjetos);
+                        ControlarPuertasYJefes.JefeEliminado = false;
+                        estadisticasDePersonaje.MurioDespuesDeMatarJefe = false;
 
-				if(ControlarPuertasYJefes.JefeDeFuegoMuerto == true)
-				{
-					GameObject PuertaDeFuego = GameObject.Find("EntradaNivelFuego");
-					PuertaDeFuego.SetActive(false);
-				}
+                    }
+                    else
+                    {
+                        ControlarPuertasYJefes.JefeEliminado = false;
+                    }
+                }
 
-				if(ControlarPuertasYJefes.JefeDeTierraMuerto == true)
-				{
-					GameObject PuertaDeTierra = GameObject.Find("EntradaNivelRoca");
-					PuertaDeTierra.SetActive(false);
-				}
 
-				if(ControlarPuertasYJefes.JefeDeVientoMuero == true)
-				{
-					GameObject PuertaDeViento = GameObject.Find("EntradaNivelViento");
-					PuertaDeViento.SetActive(false);
-				}
+                if (ControlarPuertasYJefes.JefeDeFuegoMuerto == true)
+                {
+                    GameObject PuertaDeFuego = GameObject.Find("EntradaNivelFuego");
+                    GameObject.Find("FuegoLuz").GetComponent<Light>().range = 33;
+                    PuertaDeFuego.SetActive(false);
+                }
 
-				if(ControlarPuertasYJefes.JefeDeAguaMuerto == true)
-				{
-					GameObject PuertaDeAgua = GameObject.Find("EntradaNivelAgua");
-					PuertaDeAgua.SetActive(false);
-				}
-			}
-		}
-	}
+                if (ControlarPuertasYJefes.JefeDeTierraMuerto == true)
+                {
+                    GameObject PuertaDeTierra = GameObject.Find("EntradaNivelRoca");
+                    GameObject.Find("RocaLuz").GetComponent<Light>().range = 33;
 
-	public void BuscadorDeFuncionParaGuardarItems()
-	{
-		ListasDeItems FuncionParaGuardar = FindObjectOfType<ListasDeItems>();
-		FuncionParaGuardar.LlenarElConstructor();
-	}
+                    PuertaDeTierra.SetActive(false);
+                }
 
-	public void BuscadorFuncionParaCargarItems()
-	{
-		ListasDeItems FuncionParaCargar = FindObjectOfType<ListasDeItems>();
-		FuncionParaCargar.InstanciarLosObjetosDelConstructor();
-	}
+                if (ControlarPuertasYJefes.JefeDeVientoMuero == true)
+                {
+                    GameObject PuertaDeViento = GameObject.Find("EntradaNivelViento");
+                    PuertaDeViento.SetActive(false);
+                    GameObject.Find("VientoLuz").GetComponent<Light>().range = 33;
+                }
+
+                if (ControlarPuertasYJefes.JefeDeAguaMuerto == true)
+                {
+                    GameObject PuertaDeAgua = GameObject.Find("EntradaNivelAgua");
+                    GameObject.Find("AguaLuz").GetComponent<Light>().range = 33;
+
+                    PuertaDeAgua.SetActive(false);
+                }
+                if (ControlarPuertasYJefes.JefeDeAguaMuerto && ControlarPuertasYJefes.JefeDeVientoMuero && ControlarPuertasYJefes.JefeDeTierraMuerto && ControlarPuertasYJefes.JefeDeFuegoMuerto)
+                {
+                    GameObject.Find("FinalLuz").GetComponent<Light>().range = 40;
+
+                }
+            }
+        }
+    }
+
+    public void BuscadorDeFuncionParaGuardarItems()
+    {
+        ListasDeItems FuncionParaGuardar = FindObjectOfType<ListasDeItems>();
+        FuncionParaGuardar.LlenarElConstructor();
+    }
+
+    public void BuscadorFuncionParaCargarItems()
+    {
+        ListasDeItems FuncionParaCargar = FindObjectOfType<ListasDeItems>();
+        FuncionParaCargar.InstanciarLosObjetosDelConstructor();
+    }
 }
