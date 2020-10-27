6ad7bccd42821ab038cc1946c6ee9fccb2b11fee
joacomenda <joacomenda@hotmail.com.ar>
2020-10-27 20:51:18 -0300

    Menu completado

---
+++ Xanathar/Assets/Scripts/MenuYCreditos/AnimacionDesaparecer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimacionDesaparecer : MonoBehaviour
+{
+    public Animator AnimacionIniciarJuego;
+    public void IniciarJuego()
+    { 
+        AnimacionIniciarJuego.SetBool("BotonJugarApretado" , true);
+    }
+
+    public void CambiarALobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
---
+++ Xanathar/Assets/Scripts/MenuYCreditos/BotonJugar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonJugar : MonoBehaviour
+{
+    public bool Pulsoboton = false;
+    public AnimacionDesaparecer Iniciar;
+
+    public void BotonApretado()
+    {
+        if(Pulsoboton == false)
+        {
+            Pulsoboton = true;
+            Iniciar.IniciarJuego();
+        }
+    }
+}
