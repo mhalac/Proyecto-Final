85b9db4049ac8fbcd855de2fe6c61c17bf804f21
joacomenda <joacomenda@hotmail.com.ar>
2020-10-31 01:04:07 -0300

    Juego con creditos integrado y es rejugable ahora, Bug del HUD solucionado

--- Xanathar/Assets/Scripts/MenuYCreditos/CodigoCambio.cs
+++ Xanathar/Assets/Scripts/MenuYCreditos/CodigoCambio.cs
@@ -11,4 +11,22 @@ public class CodigoCambio : MonoBehaviour
         PulsarBoton.SePresionoBoton = false;
         SceneManager.LoadScene(NombreDeEscena);
     }
+
+    public void DesactivarPersonajeYTodo()
+    {
+        GameObject Jugador = GameObject.FindGameObjectWithTag("Personaje");
+        GameObject CanvasHUD = GameObject.Find("ContenedorDeHUD2.0");
+        if(Jugador != null && CanvasHUD != null)
+        {
+            ControlarPuertasYJefes.JefeDeAguaMuerto = false;
+            ControlarPuertasYJefes.JefeDeFuegoMuerto = false;
+            ControlarPuertasYJefes.JefeDeTierraMuerto = false;
+            ControlarPuertasYJefes.JefeDeVientoMuero = false;
+
+            Cursor.lockState = CursorLockMode.Confined;
+
+            Destroy(Jugador);
+            Destroy(CanvasHUD);
+        }
+    }
 }
